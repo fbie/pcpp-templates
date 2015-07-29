428702bf9f8131142dec966494a6e279d59f1fea
Florian Biermann <fbie@itu.dk>
2015-07-29 11:05:42 +0200

    apply new structure to week01 and week02 examples

--- week01/test.sh
+++ week01/test.sh
@@ -1,1 +1,1 @@
-java -cp ./pcpp/:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore pcpp.week01.TestSubmission
+java -cp .:bin:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore pcpp.week01.TestSubmission
--- week02/test.sh
+++ week02/test.sh
@@ -1,1 +1,1 @@
-java -cp .:pcpp/week02:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore pcpp.week02.TestSubmission
+java -cp .:bin:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore pcpp.week02.TestSubmission
