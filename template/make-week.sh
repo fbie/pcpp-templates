6caaa3c61e4857cffa7be094a1a9c60906867113
Florian Biermann <fbie@itu.dk>
2015-07-29 10:58:56 +0200

    add an ant buildfile to template
    Better for Java projects since tools like Eclipse understand it.

--- template/make-week.sh
+++ template/make-week.sh
@@ -7,15 +7,19 @@ WEEK=$1
 TEMPLATE_DIR=$(cd $(dirname $0); pwd)
 
 # Test script
-TEST_SH="java -cp .:pcpp/week$WEEK:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore pcpp.week$WEEK.TestSubmission"
+TEST_SH="java -cp .:bin:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore pcpp.week$WEEK.TestSubmission"
 
 # Build the directory
 ROOT=$(pwd)/week$WEEK
-PKG=$ROOT/pcpp/week$WEEK
+PKG=$ROOT/src/pcpp/week$WEEK
 mkdir -p $PKG
 
 # Build the makefile
 echo "WEEK=$WEEK" > $ROOT/Makefile && cat $TEMPLATE_DIR/Makefile >> $ROOT/Makefile
+
+# Build the build.xml
+cp $TEMPLATE_DIR/build.xml $ROOT/build.xml
+sed -i '' s/THEWEEK/$WEEK/g $ROOT/build.xml
 
 # Copy test template and add package.
 cp $TEMPLATE_DIR/TestSubmission.java $PKG/TestSubmission.java
